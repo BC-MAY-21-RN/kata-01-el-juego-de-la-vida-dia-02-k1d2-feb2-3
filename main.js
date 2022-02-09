f806f5563bbfc32d42a24054c2dc043de4da8fea
jonathan <yoni_12rrs@hotmail.com>
2022-02-09 17:55:16 -0600

    Creación del tablero

--- main.js
+++ main.js
@@ -2,10 +2,29 @@ class Tablero {
     constructor(filas,columnas) {
         this.filas = filas;
         this.columnas = columnas;
+
+        
+        this.creartablero();
+
+    }
+    
+    creartablero() {
+
+        /* creación del tablero, creamos las primeras filas como un array */
+        let tableroX = new Array(this.columnas);
+        
+        /* Aqui el ciclo */
+        for(let i = 0;i < tableroX.length; i++){
+          
+
+            tableroX[i] = new Array(this.filas).fill('*')
+
+          
+        }
+        console.log(tableroX);
+
     }
 }
  
 
-
-/* Commit inicial */
-const tablero = new Tablero(4, 8);
+const tablero = new Tablero(8, 4);
